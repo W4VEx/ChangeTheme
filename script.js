0ee7cda6e1935217253142fb77e67fc3f4ad99d7
W4VEx <159764503+W4VEx@users.noreply.github.com>
2024-02-19 12:13:58 +0700

    ChangeTheme

---
+++ script.js
@@ -0,0 +1,16 @@
+let h1 = document.getElementById('header')
+let txt = document.getElementById('txt')
+let btn = document.getElementById('header')
+
+
+function ChangeTheme(){
+    
+  document.body.style.backgroundColor = "var(--bgdmcolor)";
+  h1.style.color = "var(--dheader)";
+  txt.style.color= "var(--dtext)";
+
+
+}
+
+
+
